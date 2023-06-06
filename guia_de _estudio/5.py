8506ebcd9f69486c6e195b5dab6d56faf3ef156e
TheGaxz <TheGaxz@users.noreply.github.com>
2023-06-05 21:17:42 -0400

    Updated

---
+++ guia_de _estudio/5.py
@@ -0,0 +1,31 @@
+'''Desarrollar un programa que permita al usuario ingresar una serie de numeros enteros 
+positivos (N numeros) hasta que ingrese -1 (Solo positivos ignorando los numeros negativos).
+Luego, el programa debe calcular e imprimir en pantalla lo siguiente: el numero 
+mayor de los ingresados, la sumatoria total de los numeros, la sumatoria de los numeros 
+pares, la sumatoria de los numeros impares y el promedio total. Ademas, se debe imprimir 
+si el numero mayor obtenido es mayor, menor o igual que el promedio calculado. 
+Asegurate de resolver este problema utilizando las funciones que consideres adecuadas.
+Nota: el -1 no se cuenta. Si el usuario ingresa un numero negativo debe volver a pedir un 
+numero y no se usa en el calculo. 
+
+El numero es mayor que el promedio y este es 9'''
+
+x = int(input("Ingrese los numeros: "))
+
+while x == -1:
+    x = int(input("Ingrese los numeros: "))
+
+
+
+#Suma de pares
+#Suma de impares
+#Suma total
+#Promedio
+#N° mayor ingresado
+
+
+
+
+
+
+
